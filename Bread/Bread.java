963a222c6ffc8d24bafee70b39cbeaa6caac86cd
Lilly <111086892+Lvore00@users.noreply.github.com>
2022-09-16 18:19:18 -0400

    Update Bread.java

--- Bread/Bread.java
+++ Bread/Bread.java
@@ -1,2 +1,88 @@
 package Bread;
 
+import javax.swing.*;
+
+public class Bread {
+    //Data
+
+    private double flour;
+    private double water;
+    private double salt;
+    private double yeast;
+    private double starter;
+
+    //Constructors
+ public Bread (){
+     this.flour = flour;
+     this.water = water;
+     this.salt = salt;
+     this.yeast = yeast;
+     this.starter = starter;
+ }
+
+    //Getters and Setters
+
+    public double getFlour() {
+        return flour;
+    }
+
+    public void setFlour(double flour) {
+        this.flour = flour;
+    }
+
+    public double getWater() {
+        return water;
+    }
+
+    public void setWater(double water) {
+        this.water = water;
+    }
+
+    public double getSalt() {
+        return salt;
+    }
+
+    public void setSalt(double salt) {
+        this.salt = salt;
+    }
+
+    public double getYeast() {
+        return yeast;
+    }
+
+    public void setYeast(double yeast) {
+        this.yeast = yeast;
+    }
+
+    public double getStarter() {
+        return starter;
+    }
+
+    public void setStarter(double starter) {
+        this.starter = starter;
+    }
+
+    //Instance Methods
+   /** public void bake(){
+     System.out.println("Bread is being baked! Not baked all the way" +
+             " yet!");
+
+    }
+   public void getIngredients(){
+       return getFlour(), getSalt(), getStarter(), getWater(), getYeast();
+
+   }
+    */
+
+
+    //String toString
+
+    public String toString() {
+        return "Bread: " +
+                flour + " cup(s) of flour"+ "\n" +
+                water + " cup(s) of water" + "\n" +
+                salt + " tsp(s) of salt" + "\n" +
+                yeast + " tsp(s) of yeast" + "\n" +
+                starter + " cup(s) of starter" ;
+    }
+}
