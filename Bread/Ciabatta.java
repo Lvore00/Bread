63b5f7cca7b33789ab8264a28a20a4e2668b0405
Lilly <111086892+Lvore00@users.noreply.github.com>
2022-09-17 19:48:19 -0400

    Update Ciabatta.java

--- Bread/Ciabatta.java
+++ Bread/Ciabatta.java
@@ -3,36 +3,35 @@ package Bread;
 public class Ciabatta extends Bread{
     //Data
 
-//    private double flour;
-//    private double water;
-//    private double salt;
-//    private double yeast;
-//    private double starter;
     private double sugar;
     private double bakingP;
     private String breadName;
     private String state;
-    private String recipe;
+
+    public Ciabatta(){
+        super(4.0,3.0,1.25,1.0,2.0);
+        setBakingP(2.0);
+        setBreadName("Ciabatta");
+        setState("Not done!");
+        setSugar(2.0);
+
+    }
 
     // Constructor
 
     public Ciabatta(double flour, double water, double salt, double yeast,
                     double starter, double sugar, double bakingP,
-                    String breadName, String state, String recipe) {
-      super(flour, water, salt, yeast, starter);
+                    String breadName, String state) {
+        super(flour, water, salt, yeast, starter);
         this.sugar = sugar;
         this.bakingP = bakingP;
         this.breadName = breadName;
         this.state = state;
-        this.recipe = recipe;
+
     }
 
     //Getters and Setters
 
-//    public void setFlour(double amount){
-//        super.setFlour(amount);
-//    }
-//
 
     public double getSugar() {
         return sugar;
@@ -66,45 +65,35 @@ public class Ciabatta extends Bread{
         this.state = state;
     }
 
-    public String getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(String recipe) {
-        this.recipe = recipe;
-    }
 
     // Instance Method
+
 
     public void howtoCiabatta(){
         System.out.println("After dough mixture is made, let the dough sit and rise " +
                 "for at least 15 to 20 hours, covered");
     }
+@Override
+    public String getRecipe(){
+        return " 1. Bake bread in a bake-able pan at 475 degrees F" + "\n"
+                + "2. Place into the oven, let bake until dark brown" +
+                "\n" + "3. Bake for 35 - 45 minutes " + "\n" +
+                "4. Let bread rest and cool overnight";
 
-    //public void recipe(){
-    // return
-//    }
-
-
-
-
-
-
+    }
+    @Override
+    public String getIngredients(){
+        return super.Ingredients() + "\nSugar: " + getSugar() +
+                "\nBaking Powder: " + getBakingP() +
+                "\nBread Name: " + getBreadName() +
+                "\nBread State: " + getState();
+    }
 
 
     // String toString
-
+@Override
     public String toString() {
-        return "Ciabatta: " +
-                super.getFlour()+ " cup(s) of flour" + "\n" +
-                super.getWater() + " cup(s) of water" + "\n" +
-                super.getSalt() + " tsp(s) of salt" + "\n" +
-                super.getYeast() + " tsp(s) of yeast" + "\n" +
-                sugar + " cup(s) of sugar" + "\n" +
-                bakingP + " tsp(s) of baking powder" + "\n" +
-                super.getStarter() + " cup(s) of ripe ciabatta starter" + "\n" +
-                "Bread Name: " + breadName + "\n" +
-                "Bread State: " + state + "\n" +
-                "Recipe: " + recipe;
+        return getIngredients() + "\n" + getRecipe() +
+                "\n" + getState();
     }
 }
