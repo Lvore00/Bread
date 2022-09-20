bbc0ccb2b1792454b43985e47ce52632ff42e0b1
Lilly <111086892+Lvore00@users.noreply.github.com>
2022-09-20 17:48:20 -0400

    Update BreadTester.java

--- Bread/BreadTester.java
+++ Bread/BreadTester.java
@@ -3,11 +3,11 @@ package Bread;
 public class BreadTester {
     public static void main(String[] args) {
         Bread bread = new Bread();
-        Bread ciabatta = new Ciabatta();
-        Bread muffins = new Muffins();
-        Bread doughnut = new Doughnut();
-        Bread pastry = new Pastry();
-        Bread sourdough = new Sourdough();
+        Ciabatta ciabatta = new Ciabatta();
+        Muffins muffins = new Muffins();
+        Doughnut doughnut = new Doughnut();
+        Pastry pastry = new Pastry();
+        Sourdough sourdough = new Sourdough();
 
         System.out.println(doughnut);
 
